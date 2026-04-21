e4cb95c51212da10f2cb75bcc9ceb760a95605d6
benjwamintennis-dotcom <benjwamintennis@gmail.com>
2026-04-20 19:17:47 -0700

    message

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
+let clicked = false;
+
 const button = document.getElementById('magicBtn');
 
 button.addEventListener('click', () => {
+    clicked = true;
+
     alert("It works! You just deployed a site from the cloud!");
-    document.body.style.background = "linear-gradient(135deg, #ff9a9e 0%, #fecfef 99%)";
+
+    document.body.style.background = "linear-gradient(135deg, pink, purple)";
 });
 
+// p5 setup
+function setup() {
+  createCanvas(400, 400);
+}
+
+// p5 draw loop
+function draw() {
+  if (clicked) {
+    background("orange");
+    ellipse(200, 200, 150, 150);
+  } else {
+    background("lightblue");
+  }
+}
