31a4be21d5353cba688b3d911ea9ebde7eb2cd6e
benjwamintennis-dotcom <benjwamintennis@gmail.com>
2026-04-26 14:30:23 -0700

    s

--- projects/album art/album-art.js
+++ projects/album art/album-art.js
@@ -1,28 +1,18 @@
+let angle = 0;
 
 function setup() {
   createCanvas(400, 400);
-
-
- 
 }
 
-let angle = 0;
-
-
-
 function draw() {
-  background(220);
-  background('Green')
-
+  background("green");
 
   translate(200, 200);
-
-  // Apply rotation
   rotate(angle);
-  
-  stroke('skyBlue')
-   strokeWeight(random(1,10))
-  // Draw the wheel
+
+  stroke("skyblue");
+  strokeWeight(random(1, 10));
+
   circle(0, 0, 300);
   line(0, 0, 0, -150);
   line(0, 0, 115, -95);
@@ -32,27 +22,22 @@ function draw() {
   line(0, 0, -115, -95);
   line(0, 0, -115, 95);
   line(0, 0, -150, 0);
-  stroke('black')
-  textSize(20)
-  //text size, thickness, and color modifications.
-  strokeWeight(1)
-  
-  
-  wilch = text('Spin', 60, -20)
-  textSize(15)
-  wilch = text('By Jahzzar', 45,30)
-  textSize(20)
-  wilch = text('Spin', -125, -20)
-  textSize(15)
-  wilch = text('By Jahzzar', -125, 30)
-  //text with song name and artist name
-  
 
+  stroke("black");
+  strokeWeight(1);
 
-  
+  textSize(20);
+  text("Spin", 60, -20);
 
-  
+  textSize(15);
+  text("By Jahzzar", 45, 30);
+
+  textSize(20);
+  text("Spin", -125, -20);
+
+  textSize(15);
+  text("By Jahzzar", -125, 30);
+
   angle += random(0.01, 1);
-  frameRate(1)
-  //slows down the speed the wheel spins at.
+  frameRate(1);
 }
