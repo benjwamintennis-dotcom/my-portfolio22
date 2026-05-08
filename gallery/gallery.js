226e38832b593680453fb886d47588e16d18d47d
benjwamintennis-dotcom <benjwamintennis@gmail.com>
2026-05-07 21:51:43 -0700

    y

--- gallery/gallery.js
+++ gallery/gallery.js
@@ -5,14 +5,17 @@ let imgElement;
 function setup() {
   noCanvas();
   imgElement = select("#gallery-image");
+
   let button = select("#gallery-button");
   button.mousePressed(changeImage);
 }
 
 function changeImage() {
   currentImageIndex = currentImageIndex + 1;
+
   if (currentImageIndex >= images.length) {
     currentImageIndex = 0;
   }
+
   imgElement.attribute("src", images[currentImageIndex]);
 }
